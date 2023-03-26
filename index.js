3a14d52c54d373e58442dd70f21c2b4e1ea85dfa
daniel <daniel.a.altman@gmail.com>
2023-03-26 16:03:44 -0700

    Add debug messages + fix chat history + command handler

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const rule_gameplay = formatInputChunk("Gameplay", gameplay);
 const rule_party = formatInputChunk("Player Characters", party);
 const rule_story = formatInputChunk("Story", story);
 const prompt = input.concat(rule_gameplay, rule_party, rule_story);
+
+// DEBUG
+console.log("##### PROMPT");
 console.log(prompt);
+console.log("##### END PROMPT");
 
 // create discord bot using open AI apis
 require("dotenv").config();
@@ -47,8 +51,74 @@ InitMessages();
 // Create Pause var
 client.isPaused = false;
 
+// check if input is number
 function isNumeric(str) {
     return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
+function stripExclamationMark(input) {
+    if (input.endsWith("!")) {
+        return input.slice(0, -1);
+    }
+    return input;
+}
+
+// Split message function
+function splitMessage(resp, charLim) {
+    const responseNum = Math.ceil(resp.length / charLim);
+    const responses = new Array(responseNum);
+    // For the number of split responses, if its the last response, make the size the character limit, else make the size the last index of a space that is under 2000 characters
+    for (
+        let i = 0, c = 0, chunkSize = null;
+        i < responseNum;
+        i++, c += chunkSize
+    ) {
+        if (i + 1 >= responseNum) {
+            chunkSize = charLim;
+        } else {
+            chunkSize = resp.substr(c, charLim).lastIndexOf(" ");
+        }
+        responses[i] = resp.substr(c, chunkSize);
+    }
+    return responses;
+}
+
+// Implement a command handler
+const commands = {
+    "!start": startCommand,
+    "!pause": pauseCommand,
+    "!reset": resetCommand,
+    "!stop": stopCommand,
+};
+
+async function handleCommand(command, message) {
+    if (commands[command]) {
+        await commands[command](message);
+    } else {
+        message.reply(`Unknown command: ${command}`);
+    }
+}
+
+async function startCommand(message) {
+    client.isPaused = false;
+    message.reply(`DmBot has started`);
+    SendCustomMessage(message, go);
+}
+
+async function pauseCommand(message) {
+    client.isPaused = true;
+    message.reply(`DmBot has paused`);
+}
+
+async function resetCommand(message) {
+    InitMessages();
+    message.reply(`DmBot has reset`);
+}
+
+async function stopCommand(message) {
+    InitMessages();
+    client.isPaused = true;
+    message.reply(`DmBot has stopped`);
 }
 
 async function SendMessage(message) {
@@ -56,20 +126,39 @@ async function SendMessage(message) {
     message.channel.sendTyping();
 
     // send query and return response
-    const generatedText = await sendQueryReturnResponse(message);
+    const generatedText = await sendQueryReturnResponse();
 
-    // reply with the latest message content
+    // DEBUG
+    console.log("##### RESPONSE");
     console.log(generatedText);
-    message.channel.send(generatedText);
+    console.log("##### END RESPONSE");
+
+    // Split response if it exceeds the Discord 2000 character limit
+    const responseChunks = splitMessage(generatedText, 2000);
+
+    // Send the split API response
+    for (let i = 0; i < responseChunks.length; i++) {
+        message.channel.send(responseChunks[i]);
+    }
 }
 
-async function sendQueryReturnResponse(message) {
+async function SendCustomMessage(message, input) {
+    message.content = input;
+    messages.push({ role: "user", content: `${message.content}` });
+    await SendMessage(message);
+}
+
+async function sendQueryReturnResponse() {
+    // DEBUG
+    console.log("##### REQUEST");
+    console.log(messages);
+    console.log("##### END REQUEST");
+
     // send a request using the open ai api
     const response = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: messages,
     });
-    console.log("sending discord message..");
 
     // store generated text and append with bot reply
     const generatedText = response.data.choices[0].message.content;
@@ -78,9 +167,32 @@ async function sendQueryReturnResponse(message) {
     // DEBUG ECHO
     // message.reply(`You said: ${message.content}`);
 
-    console.log(messages);
-    console.log(generatedText);
+    // console.log(messages);
+    // console.log(generatedText);
     return generatedText;
+}
+
+function shouldSendMessage(message) {
+    // Check if the message ends with "!" or "?", or if the message is a number or a "!dm" command
+    const doesMessageExclaim = message.content.endsWith("!");
+    const isMessageQuestion = message.content.endsWith("?");
+    const isMessageNumber = isNumeric(message.content);
+    const isDmCommand = message.content == "!dm";
+
+    return (
+        isDmCommand ||
+        doesMessageExclaim ||
+        isMessageQuestion ||
+        isMessageNumber
+    );
+}
+
+function handleError(err) {
+    console.error("Error:", err.message);
+    if (err.response && err.response.data) {
+        console.error("Response data:", err.response.data);
+    }
+    console.log(err);
 }
 
 // check for messages on discord when sent
@@ -89,71 +201,35 @@ client.on("messageCreate", async function (message) {
         // don't respond to bots (including self)
         if (message.author.bot) return;
 
-        if (message.content == "!start") {
-            client.isPaused = false;
-            message.reply(`DmBot has started`);
+        // check for commands
+        const command = message.content.split(" ")[0];
+        if (commands.hasOwnProperty(command)) {
+            await handleCommand(command, message);
             return;
         }
 
-        if (message.content == "!pause") {
-            client.isPaused = true;
-            message.reply(`DmBot has paused`);
-            return;
-        }
-
-        if (message.content == "!reset") {
-            InitMessages();
-            message.reply(`DmBot has reset`);
-            return;
-        }
-
-        if (message.content == "!stop") {
-            client.isPaused = true;
-            InitMessages();
-            message.reply(`DmBot has stopped`);
-            return;
-        }
-
+        //check if bot is paused
         if (client.isPaused) {
-            message.reply(`DmBot hasn't been started yet`);
-            console.log("is paused");
+            console.log("DmBot is paused");
             return;
         }
 
-        // continue the story under these conditions
-        // the message ends with "!"
-        const doesMessageExclaim = message.content.endsWith("!");
-        if (doesMessageExclaim) {
-            console.log("The message ends with an exclamation mark!");
-            message.content = message.content.slice(0, -1);
+        // Check if the message content is not "!dm" before appending it to the message history
+        if (message.content !== "!dm") {
+            // strip exclamation mark from ending
+            const user_input = stripExclamationMark(message.content);
+
+            // push new message to chat history
+            messages.push({ role: "user", content: `${user_input}` });
         }
 
-        // the message ends with "?"
-        const isMessageQuestion = message.content.endsWith("?");
-        if (isMessageQuestion) {
-            console.log("The message ends with an question mark?");
-        }
-
-        const isMessageNumber = isNumeric(message.content);
-        const isDmCommand = message.content == "!dm";
-        if (
-            isDmCommand ||
-            doesMessageExclaim ||
-            isMessageQuestion ||
-            isMessageNumber
-        ) {
-            SendMessage(message);
+        //check to send message
+        if (shouldSendMessage(message)) {
+            await SendMessage(message);
             return;
         }
-
-        // append user message to message history
-        messages.push({ role: "user", content: `${message.content}` });
     } catch (err) {
-        console.error("Error:", err.message);
-        if (err.response && err.response.data) {
-            console.error("Response data:", err.response.data);
-        }
-        console.log(err);
+        handleError(err);
     }
 });
 
