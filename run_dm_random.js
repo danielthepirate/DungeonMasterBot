6656d00d51c30f26d266e4fc42a4d498b21709ff
daniel <daniel.a.altman@gmail.com>
2023-03-28 19:34:05 -0700

    New prompts

--- run_dm_random.js
+++ run_dm_random.js
@@ -1,3 +1,3 @@
 const go =
-    "Start by describing the scene. Then describe each character and what each character is doing. Then describe any points of interest or notable features. Then list each character with their options.";
+    "Start by providing a brief summary of what the players know about their quest. Then in a new paragraph, start describing the scene. Then describe each character and what each character is doing. Then describe any points of interest or notable features. Then list each character with their options. Thanks and let's have fun together!";
 module.exports = go;
